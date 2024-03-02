cab52d145e096314aa73dfdddec2510a98270673
Bang.Voluong <vo.luong.bang@sun-asterisk.com>
2024-03-03 02:00:11 +0900

    #2 add logger middleware

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	fmt.Print("testing")
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
-		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Addr:    ":3000",
+		Handler: router,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
